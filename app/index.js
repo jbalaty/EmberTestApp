883195ea4962874f0f4db2e1b3dc62d0bedfca61
Jiri Balaty <jiri.balaty@gmail.com>
2013-05-10 14:53:12 +0200

    Upraveny CSS

--- app/index.js
+++ app/index.js
@@ -1,12 +1,45 @@
-var connect = require('connect'),
-    http = require('http');
+#!/usr/bin/env node
+"use strict"
 
-//starting server on port 
-var port = 3001;
-console.log("Creating connect server on port: "+port);
-server = connect.createServer(
-    connect.favicon()
-  , connect.logger()
-  , connect.static(__dirname + '/public')
-)
-server.listen(port)
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    path = require('path'),
+    http = require('http')
+
+var app = module.exports = express();
+
+
+// Configuration
+app.configure(function () {
+    app.use(express.logger());
+//  app.use(express.bodyParser());
+//  app.use(express.methodOverride());
+    app.use(express.static(path.join(__dirname, '/public')));
+});
+
+
+var staticExpiration = 1000;
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+});
+
+app.configure('production', function () {
+    staticExpiration = 86400000;
+    app.use(express.errorHandler());
+});
+
+app.get("/", function (req, res) {
+    //res.redirect("public/index.html");
+    res.send('See this page: <a href="public/index.html">index.html</a>');
+});
+
+
+var port = 8004;
+var hostname = '127.0.0.1';
+app.listen(port);
+console.log("Express server listening on http://%s:%d", hostname, port);
+
